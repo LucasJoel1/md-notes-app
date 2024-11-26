6fd234c8b734ab3f90fc92b9b13915b72546d1f0
LucasJoel1 <ljoel1@outlook.com>
2024-11-26 16:24:30 -0500

    first commit

---
+++ src/templates/files.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <main>
+        <div class="fileContainer" id="fileView">
+            <div class="fileContainerResizer" id="fileViewResizer">
+
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            let resizer = document.getElementById('fileViewResizer')
+            let fileView = document.getElementById("fileView")
+            resizer.addEventListener("mouseover", (event) => {
+                console.log("e")
+                document.addEventListener("mousedown", (e) => {
+                    console.log("a")
+                    fileView.style.width = event.pageX - fileView.getBoundingClientRect().left + 'px';
+                    console.log()
+                })
+            })
+        }
+    }
+</script>
